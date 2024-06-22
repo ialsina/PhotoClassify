4dd09218d6e274d3447225b96618dcea2142ccb1
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-06-22 20:51:09 +0200

    Update: Use hashing to compare files, create higher-level functions

--- diff.py
+++ diff.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import hashlib
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
-from typing import Callable, Sequence
+from typing import Callable, Sequence, Optional
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,15 +32,22 @@ class RelationPath(Path):
         self._twins = value
 
 
-def _get_paths():
+def _get_paths(
+    origin: Optional[Path] = None,
+    destination: Optional[Path] = None
+):
+    if origin is None:
+        origin = cfg.path.origin
+    if destination is None:
+        destination = cfg.path.destination
     paths1 = []
     paths2 = []
     print("Collecting paths...")
     try:
-        for root, _, files in cfg.path.origin.walk():
+        for root, _, files in origin.walk():
             for file in files:
                 paths1.append(RelationPath(root / file))
-        for root, _, files in cfg.path.destination.walk():
+        for root, _, files in destination.walk():
             for file in files:
                 paths2.append(root / file)
     except Exception as exc: # pylint: disable=W0718
@@ -55,7 +63,7 @@ def _same_size(path1: Path, path2: Path):
 def _same_ctime(path1: Path, path2: Path):
     return path1.stat().st_ctime == path2.stat().st_ctime
 
-def find_add_candidates(
+def _find_add_candidates(
     paths1: Sequence[RelationPath],
     paths2: Sequence[Path],
     *funs: Callable[[Path, Path], bool]
@@ -78,7 +86,7 @@ def find_add_candidates(
             candidates.append(dest_path)
         fpath.candidates = candidates
 
-def add_twins_parallel(paths1: Sequence[RelationPath]) -> None:
+def _add_twins_parallel(paths1: Sequence[RelationPath], max_workers: Optional[int] = None) -> None:
     """
     Find and add twin files using parallel processing.
 
@@ -87,7 +95,7 @@ def add_twins_parallel(paths1: Sequence[RelationPath]) -> None:
         paths2 (Sequence[Path]): List of destination file paths.
     """
     print("Finding twins in parallel...")
-    with ProcessPoolExecutor(max_workers=8) as executor:
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
         futures = {
             executor.submit(compare_files, p1, p2): (p1, p2)
             for p1
@@ -103,7 +111,7 @@ def add_twins_parallel(paths1: Sequence[RelationPath]) -> None:
 
 def compare_files(file1: Path, file2: Path) -> bool:
     """
-    Compare two files byte-by-byte.
+    Compare two files by their SHA256 hash.
 
     Args:
         file1 (Path): The first file to compare.
@@ -112,13 +120,80 @@ def compare_files(file1: Path, file2: Path) -> bool:
     Returns:
         bool: True if files are identical, False otherwise.
     """
-    with open(file1, "rb") as f1, open(file2, "rb") as f2:
-        return f1.read() == f2.read()
+    return calculate_file_hash(file1) == calculate_file_hash(file2)
 
-def save_histograms(
+
+def calculate_file_hash(filepath: Path) -> str:
+    """
+    Calculate the SHA256 hash of a file.
+
+    Args:
+        filepath (Path): Path to the file.
+
+    Returns:
+        str: SHA256 hash of the file.
+    """
+    sha256 = hashlib.sha256()
+    with open(filepath, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
+def find_files_with_copy(origin: Path, destination: Path, parallel=True, max_workers=None):
+    """
+ Find files in the origin directory that have identical copies in the destination directory.
+
+    Parameters
+    ----------
+    origin : Path
+        Path to the origin directory.
+    destination : Path
+        Path to the destination directory.
+    parallel : bool, optional
+        Whether to use parallel processing, default is True.
+    max_workers : int or None, optional
+        Maximum number of worker processes for parallel processing, default is None.
+
+    Returns
+    -------
+    list of Path
+        List of Path objects representing files in the origin directory that have copies in the destination directory.
+    """
+    paths_origin, paths_destination = _get_paths(origin, destination)
+    _find_add_candidates(paths_origin, paths_destination, _same_name, _same_size)
+    _add_twins_parallel(paths_origin, max_workers=(max_workers if parallel else 1))
+    paths_origin_with_copy = [Path(p) for p in paths_origin if any(p.twins)]
+    return paths_origin_with_copy 
+
+def find_files_without_copy(origin: Path, destination: Path, parallel=True, max_workers=None):
+    """
+    Find files in the origin directory that do not have identical copies in the destination directory.
+
+    Parameters
+    ----------
+    origin : Path
+        Path to the origin directory.
+    destination : Path
+        Path to the destination directory.
+    parallel : bool, optional
+        Whether to use parallel processing, default is True.
+    max_workers : int or None, optional
+        Maximum number of worker processes for parallel processing, default is None.
+
+    Returns
+    -------
+    list of Path
+        List of Path objects representing files in the origin directory that do not have copies in the destination directory.
+    """
+    paths_origin, _ = _get_paths(origin, destination)
+    paths_with_copy = find_files_with_copy(
+        origin=origin, destination=destination, parallel=parallel, max_workers=max_workers
+    )
+    return [path for path in paths_origin if path not in paths_with_copy]
+
+def _make_histogram(
     paths1,
     paths2,
-    filepath="hists.png",
     nbins=100,
     split_input=True,
     filter_output=True,
@@ -151,13 +226,57 @@ def save_histograms(
     ax[1].set_xlabel("Size (MB)")
     ax[0].set_ylabel("File count (input)")
     ax[1].set_ylabel("File count (output)")
-    fig.savefig(filepath)
+    return fig
+
+
+def make_histogram(
+    origin: Path,
+    destination: Path,
+    fname: str | Path,
+    nbins=100,
+    split_input=True,
+    filter_output=True,
+    stacked=True,
+):
+    """
+    Create and save histograms of file sizes for files in the origin and destination directories.
+
+    Parameters
+    ----------
+    origin : Path
+        Path to the origin directory.
+    destination : Path
+        Path to the destination directory.
+    fname : str or Path
+        File path where the histogram image will be saved.
+    nbins : int, optional
+        Number of bins for the histogram, default is 100.
+    split_input : bool, optional
+        Whether to split input files into twins and non-twins, default is True.
+    filter_output : bool, optional
+        Whether to filter output files, default is True.
+    stacked : bool, optional
+        Whether to stack histograms, default is True.
+    """
+    paths_origin, paths_destination = _get_paths(origin, destination)
+    _find_add_candidates(paths_origin, paths_destination, _same_name, _same_size)
+    _add_twins_parallel(paths_origin)
+    fig = _make_histogram(
+        paths_origin,
+        paths_destination,
+        nbins=nbins,
+        split_input=split_input,
+        filter_output=filter_output,
+        stacked=stacked,
+    )
+    fig.savefig(fname)
+
 
 if __name__ == "__main__":
     paths_orig, paths_dest = _get_paths()
-    find_add_candidates(paths_orig, paths_dest, _same_name, _same_size)
-    add_twins_parallel(paths_orig)
-    save_histograms(paths_orig, paths_dest, split_input=True, filter_output=True)
+    _find_add_candidates(paths_orig, paths_dest, _same_name, _same_size)
+    _add_twins_parallel(paths_orig)
+    _make_histogram(paths_orig, paths_dest, split_input=True, filter_output=True)
     with_candidates = [p for p in paths_orig if any(p.candidates)]
     without_candidates = [p for p in paths_orig if not any(p.candidates)]
     with_twins = [p for p in paths_orig if any(p.twins)]
