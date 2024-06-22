6b704cf2d4c0a39295c044c15f07fb101178cc66
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-06-22 10:49:53 +0200

    Refactor, style and document

--- main.py
+++ main.py
@@ -1,132 +1,210 @@
-#! /usr/bin/python3
-import os
+#!/usr/bin/python3
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from collections import defaultdict
+from pathlib import Path
 import shutil
+from typing import Callable, Dict, List, Optional
 
 from tqdm import tqdm
-
 from config import config as cfg, write_date
 
-# from config import PATH_ORIGIN, PATH_DESTIN, \
-#     DAY_STARTS_AT, PROCESS_AFTER, INCLUDE_FIRST, REMOVE_FROM_SD, \
-#     AUTO_DATE, VERBOSE
 
+@dataclass
+class CopyResult:
+    """
+    Data class to store the results of a file copy operation.
 
-def get_filepaths(first_date):
+    Attributes
+    ----------
+    total : Dict[str, str]
+        A dictionary where keys are origin file paths and values are dates.
+    successful : List[str]
+        A list of successfully copied file paths.
+    existing : List[str]
+        A list of file paths that already existed in the destination.
+    unsuccessful : List[str]
+        A list of file paths that failed to copy.
+    exceptions : List[Exception]
+        A list of exceptions encountered during copying.
+    """
+    total: Dict[str, str]
+    successful: List[str]
+    existing: List[str]
+    unsuccessful: List[str]
+    exceptions: List[Exception]
+
+    def report(
+        self,
+        verbose: int = 3,
+        stdout: Callable = print,
+    ):
+        """
+        Prints a report of the copy operation results.
+
+        Parameters
+        ----------
+        verbose : int, optional
+            Level of verbosity for the report (default is 3). The levels are:
+                - 1: Basic termination message.
+                - 2: Include counts of successful, existing, and unsuccessful copies.
+                - 3: Include details of existing and unsuccessful files, and errors encountered.
+        stdout : Callable, optional
+            Function to use for printing the report (default is `print`).
+        """
+        total_len = len(self.total)
+        stdout('Copy terminated')
+        if verbose >= 2:
+            stdout(f'Successful copies: {len(self.successful)} out of {total_len}')
+            if len(self.existing) > 0:
+                stdout('***')
+                stdout(f'Existing files: {len(self.existing)} out of {total_len}')
+        if verbose >= 3:
+            stdout('\n\t'.join(self.existing))
+        if verbose >= 2:
+            if len(self.unsuccessful) > 0:
+                stdout('***')
+                stdout(f'Unsuccessful copies: {len(self.unsuccessful)} out of {total_len}')
+                stdout('\n\t'.join(self.unsuccessful))
+        if verbose >= 3:
+            stdout('> Errors encountered:')
+            stdout('\n\t'.join([str(e) for e in self.exceptions]))
+        stdout('***')
+
+def get_filepaths(first_date: Optional[datetime] = None):
+    """
+    Retrieves image file paths from the origin directory,
+    classifies them by creation date, and stores them in a dictionary.
+
+    Parameters
+    ----------
+    first_date : datetime, optional
+        The earliest date to consider for classification.
+        If not passed, taken from the configuration.
+
+    Returns
+    -------
+    Tuple[List[str], Dict[str, List[str]]]
+        A tuple containing:
+            - imgpaths: A list of all image file paths found.
+            - imgdates: A dictionary where keys are dates (as strings in 'YYYYMMDD' format)
+                and values are lists of image file paths.
+    """
+    if first_date is None:
+        first_date = cfg.date.first_date
     imgpaths = []
+    for directory in cfg.path.origin.iterdir():
+        if directory.is_dir():
+            imgpaths.extend(
+                str(file) for file in directory.iterdir() if file.is_file()
+            )
 
-    for directory in os.listdir(cfg.path.origin):
-        path = os.path.join(cfg.path.origin, directory)
-        files = os.listdir(path)
-
-        for file in files:
-            imgpaths.append(os.path.join(path, file))
-
-    # KEYS: date. VALUES: list of origin_filepath
     imgdates = defaultdict(list)
-
     for imgpath in imgpaths:
-        dt = datetime.fromtimestamp(os.path.getctime(imgpath))
+        dt = datetime.fromtimestamp(Path(imgpath).stat().st_ctime)
         modified_dt = dt - timedelta(hours=cfg.date.day_starts_at)
-
-        # Pictures too old to classify (ideally already classified)cd
+        # Pictures too old to classify (ideally already classified)
         if modified_dt < first_date:
             continue
-
-        datestamp = modified_dt.strftime('%Y%m%d')
-
+        datestamp = modified_dt.strftime(r"%Y%m%d")
         imgdates[datestamp].append(imgpath)
-
     return imgpaths, imgdates
 
 
 def create_directories(imgdates):
+    """
+    Creates directories for each date key in the imgdates dictionary.
+
+    Parameters
+    ----------
+    imgdates : Dict[str, List[str]]
+        A dictionary where keys are dates and values are lists of image file paths.
+
+    Raises
+    ------
+    FileNotFoundError
+        If a directory cannot be created.
+    """
     for datestamp in imgdates.keys():
+        target_path = cfg.path.destination + datestamp
+        if target_path.exists():
+            return
         try:
-            os.mkdir(os.path.join(cfg.path.destination, datestamp))
-        except FileExistsError:
-            pass
+            target_path.mkdir()
         except FileNotFoundError as exc:
-            print("Could not create file:\n{}/{}".format(cfg.path.destination, datestamp))
-            raise exc
+            raise FileNotFoundError(
+                "Could not create file: " + str(cfg.path.destination / datestamp)
+            ) from exc
 
 
 def copy(imgdates):
+    """
+    Copies image files to a destination path organized by date.
 
+    Parameters
+    ----------
+    imgdates : Dict[str, List[str]]
+        A dictionary where keys are dates and values are lists of origin file paths.
+
+    Returns
+    -------
+    CopyResult
+        A dataclass containing the results of the copy operation.
+    """
     successful = []
     existing = []
     unsuccessful = []
-    exceptions_set = set()
+    exceptions = set()
 
     # KEYS: origin_filepath. VALUES: date
-    imgdates2 = {origin_fpath: date for date, origin_fpaths in imgdates.items() for origin_fpath in origin_fpaths}
+    imgdates2 = {
+        origin_fpath: date
+        for date, origin_fpaths
+        in imgdates.items()
+        for origin_fpath
+        in origin_fpaths
+    }
 
     # values of imgdates are lists
-    with tqdm(total = len(imgdates2)) as progress:
+    with tqdm(total = len(imgdates2)) as pbar:
         for origin_fpath, datestamp in imgdates2.items():
-            fname = os.path.basename(origin_fpath)
-            destin_path = os.path.join(cfg.path.destination, datestamp)
-            destin_fpath = os.path.join(destin_path, fname)
-            progress.set_description_str('{:>8s}/{:<12s}'.format(datestamp, fname))
-            progress.update()
+            fname = Path(origin_fpath).name
+            destin_path = cfg.path.destination / datestamp
+            destin_fpath = destin_path / fname
+            pbar.set_description_str(
+                f"{datestamp:>8s}/{fname:<12s}"
+            )
+            pbar.update()
 
-            if not os.path.exists(destin_fpath):
+            if not destin_fpath.exists():
                 try:
                     shutil.copy2(origin_fpath, destin_path)
                     successful.append(origin_fpath)
-                except Exception as e:
+                except (FileNotFoundError, PermissionError, OSError) as exc:
                     unsuccessful.append(origin_fpath)
-                    exceptions_set.add(e)
+                    exceptions.add(exc)
             else:
                 existing.append(origin_fpath)
 
-    exceptions = list(exceptions_set)
-
-    return imgdates2, successful, existing, unsuccessful, exceptions
-
-
-def report(total, successful, existing, unsuccessful, exceptions):
-
-    total_len = len(total)
-
-    print('Copy terminated')
-
-    if cfg.copy.verbose >= 2:
-        print('Successful copies: {} out of {}'.format(len(successful), total_len))
-        if len(existing) > 0:
-            print('***')
-            print('Existing files: {} out of {}'.format(len(existing), total_len))
-    if cfg.copy.verbose >= 3:
-        print('\n\t'.join(existing))
-    if cfg.copy.verbose >= 2:
-        if len(unsuccessful) > 0:
-            print('***')
-            print('Unsuccessful copies: {} out of {}'.format(len(unsuccessful), total_len))
-            print('\n\t'.join(unsuccessful))
-    if cfg.copy.verbose >= 3:
-        print('> Errors encountered:')
-        print('\n\t'.join([str(e) for e in exceptions]))
-
-    print('***')
-
-
-def main():
-    first_date = cfg.date.first_date
-    _, image_dates = get_filepaths(first_date)
-    create_directories(image_dates)
-    if cfg.copy.verbose >= 1:
-        print('Copying files:\n'
-              '\t         FROM: {:<30s}\n'
-              '\t           TO: {:<30s}\n'
-              '\tSTARTING DATE: {:<30s}'.format(cfg.path.origin,
-                                                cfg.path.destination,
-                                                first_date.date().strftime("%a %d/%m/%Y"))
-                                                )
-    result_tuple = copy(image_dates)
-    report(*result_tuple)
-    if cfg.date.auto_date:
-        write_date()
+    return CopyResult(
+        total=imgdates2,
+        successful=successful,
+        existing=existing,
+        unsuccessful=unsuccessful,
+        exceptions=sorted(exceptions, key=str)
+    )
 
 if __name__ == "__main__":
-    main()
+    _, image_dates = get_filepaths()
+    create_directories(image_dates)
+    if cfg.copy.verbose >= 1:
+        print(
+            'Copying files:\n'
+            f'\t         FROM: {cfg.path.origin:<30s}\n'
+            f'\t           TO: {cfg.path.destination:<30s}\n'
+            f'\tSTARTING DATE: {cfg.date.first_date:<30s}'
+        )
+    copy_result = copy(image_dates)
+    copy_result.report(verbose=cfg.copy.verbose)
+    if cfg.date.auto_date:
+        write_date()
