7037af9818f7d04107c35a60eac6368cfbdf732e
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-06-23 09:43:43 +0200

    Update: Make package installable

---
+++ photoclassify/__init__.py
@@ -0,0 +1,6 @@
+from .copy import copy
+from .diff import (
+    find_files_with_copy,
+    find_files_without_copy,
+    make_histogram,
+)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='PhotoClassify',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+        ]
+    }
+)
